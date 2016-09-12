899e68d49ad0a3501fd5e04988b36d73349d47bb
TejeshShivanna <tejeshps@gmail.com>
2016-09-11 23:59:27 -0700

    Merge two sorted lists

--- src/linkedlist/LinkedlistPrograms.java
+++ src/linkedlist/LinkedlistPrograms.java
@@ -281,4 +281,51 @@ public class LinkedlistPrograms {
         prev.next = shead.next;
         return dhead.next;
     }
+    
+    // public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        ListNode head = new ListNode(-1);
+        head.next = null;
+        ListNode temp = head;
+        
+        while(l1!=null && l2!=null){
+            if(l1.val<=l2.val){
+                temp.next=l1;
+                l1 = l1.next;
+                temp = temp.next;
+            }
+            else{
+                temp.next=l2;
+                l2 = l2.next;
+                temp = temp.next;
+            }
+        }
+        if(l1==null) temp.next = l2;
+        else temp.next = l1;
+        return head.next;
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        ListNode head = new ListNode(-1);
+        head.next = null;
+        ListNode temp = head;
+        
+        while(l1!=null && l2!=null){
+            if(l1.val<=l2.val){
+                temp.next=l1;
+                l1 = l1.next;
+                temp = temp.next;
+            }
+            else{
+                temp.next=l2;
+                l2 = l2.next;
+                temp = temp.next;
+            }
+        }
+        if(l1==null) temp.next = l2;
+        else temp.next = l1;
+        return head.next;
+    }
 }
